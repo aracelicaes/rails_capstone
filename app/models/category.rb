c8bbb90f96b9cb6ab978ed70ac514b8726c588cc
aracelicaes <aracelicaes@gmail.com>
2020-10-14 23:54:28 -0500

    Adding Category into Article Views Adding Category drop down menu in article/_form partial Ading Category name in article/show Adding validation to category name length limits.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
   has_many :articles, -> { order 'created_at DESC' }
-  validates :name, uniqueness: true, presence: true
-  validates :priority, presence: true, numericality: { only_integer: true }
+
+  validates :name,
+            uniqueness: true,
+            presence: true,
+            length: { in: 3..25 }
+  validates :priority,
+            presence: true,
+            numericality: { only_integer: true }
 end
