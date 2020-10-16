9d8eeed2d3e9dc54b3511bd7ab891c4250ee1835
aracelicaes <aracelicaes@gmail.com>
2020-10-15 20:00:44 -0500

    Created content in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,97 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-# maternidad = Category.create(name, prioridad: 1)
-# sciencias 
-# technologia
-#
+Category.destroy_all
+User.destroy_all
+Article.destroy_all
+
+Category.create! ([{
+  name: 'Programming',
+  priority: 1
+},
+{
+  name: 'Science',
+  priority: 2
+},
+{
+  name: 'Motherhood',
+  priority: 3
+},
+{
+  name: 'Art & Culture',
+  priority: 4
+},
+{
+  name: 'Health',
+  priority: 5
+}])
+
+User.create! ([{
+  name: 'Lola Chapman',
+  username: 'Lola',
+  email: 'lola@foo.com',
+  password: '123456',
+  password_confirmation: '123456'
+},
+{
+  name: 'Tony Copacabana',
+  username: 'Tony',
+  email: 'tony@foo.com',
+  password: '123456',
+  password_confirmation: '123456' 
+},
+{
+  name: 'Barry Manilow',
+  username: 'Barry',
+  email: 'barry@foo.com',
+  password: '123456',
+  password_confirmation: '123456'
+}])
+
+3.times do |index|
+  Article.create!(author: User.find_by(username: 'Lola'), 
+                title: Faker::Lorem.sentence(word_count: 3),
+                text: Faker::Lorem.paragraphs(number: 5),
+                category: Category.find_by(priority: 1)
+  )
+end
+
+3.times do |index|
+  Article.create!(author: User.find_by(username: 'Tony'),
+                title: Faker::Lorem.sentence(word_count: 3),
+                text: Faker::Lorem.paragraphs(number: 6),
+                category: Category.find_by(priority: 2)
+  )
+end
+
+3.times do |index|
+  Article.create!((author: User.find_by(username: 'Barry'),
+                title: Faker::Lorem.sentence(word_count: 3),
+                text: Faker::Lorem.paragraphs(number: 6),
+                category: Category.find_by(priority: 3)
+)
+end
+
+3.times do |index|
+  Article.create!((author: User.find_by(username: 'Lola'),
+                title: Faker::Lorem.sentence(word_count: 3),
+                text: Faker::Lorem.paragraphs(number: 6),
+                category: Category.find_by(priority: 4)
+)
+end
+
+3.times do |index|
+  Article.create!((author: User.find_by(username: 'Tony'),
+                title: Faker::Lorem.sentence(3, false, 0).chop,
+                text: Faker::Lorem.paragraphs(number: 6),
+                category: Category.find_by(priority: 5)
+)
+end
+
+
+p "Created #{Category.count} categories."
+p "Created #{User.count} users."
+p "Created #{Article.count} articles."
+
