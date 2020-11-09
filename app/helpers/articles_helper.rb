a11aa0803d042fd6d66447dafaaa0c1f38454fbf
aracelicaes <aracelicaes@gmail.com>
2020-11-09 15:18:51 -0600

    Addint Styles for Article Edit

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+
+  def edit_by_author(article)
+    content_tag(:div, class: 'edit_show') do
+      content_tag(:i, class: 'fa fa-pencil', aria: { hidden: true } ) do
+        link_to edit_article_path(@article) if current_user.id == @article.author.id do
+          'Edit My Article'
+        end
+      end
+    end
+  end
+
 end
