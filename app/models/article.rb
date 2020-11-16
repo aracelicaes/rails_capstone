e2ca3e525cde7db0fcd1e6e27992a916b163f5a9
aracelicaes <aracelicaes@gmail.com>
2020-11-16 01:42:17 -0600

    Adding dependent: destroy on Votes inside Article Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,7 @@ class Article < ApplicationRecord
   has_one_attached :image
   belongs_to :author, class_name: 'User'
   belongs_to :category
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   scope :most_voted, -> { joins(:votes).group('articles.id').order('votes.count DESC').limit(1) }
 end
