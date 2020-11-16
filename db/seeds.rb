a29b23a0b23e60d86ba5ecb161121385669628ca
aracelicaes <aracelicaes@gmail.com>
2020-11-15 23:45:04 -0600

    Reset Db, Setup Seed file after cleanse.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,6 @@
 
 Category.destroy_all
 User.destroy_all
-Article.destroy_all
-Vote.destroy_all
 
 Category.create! ([{
   name: 'Programming',
@@ -54,47 +52,5 @@ User.create! ([{
   password_confirmation: '123456'
 }])
 
-# 3.times do |index|
-#   article = Article.create!(author: User.find_by(username: 'Lola'), 
-#                 title: 'Faker::Lorem.sentence(word_count: 3)',
-#                 text: Faker::Lorem.paragraphs(number: 5),
-#                 category: Category.find_by(priority: 1)
-#   )
-#   article.image.attach(io: File.open('/app/assets/images/'), filename: 'tapatia.jpg')
-#   article.save
-# end
-
-Article.create! ([{
-  title: 'Effort, Persistance, & Strength: My Tale as a Ballerina Mexicana.',
-  text: lorem_filler,
-  author: User.first,
-  category: Category.find_by(name: 'Art & Culture')
-},
-{
-  title: 'Programming: The Best Career Option For Digital Nomads?',
-  text: lorem_filler,
-  author: User.first,
-  category: Category.find_by(name: 'Programming')
-},
-{
-  title: 'Why Young Girls Should Learn How To Code',
-  text: lorem_filler,
-  author: User.find(2),
-  category: Category.find_by(name: 'Programming')
-},
-{
-  title: 'Top 5 Online Communities for Latina Programmers.',
-  text: lorem_filler,
-  author: User.find(3),
-  category: Category.find_by(name: 'Programming')
-}])
-
-Vote.create! {
-  user: User.find(3)
-  article: Article.find_by title: 'Effort, Persistance, & Strength: My Tale as a Ballerina Mexicana.',
-}
-
 p "Created #{Category.count} categories."
 p "Created #{User.count} users."
-p "Created #{Article.count} articles."
-p "Created #{Vote.count} votes."
