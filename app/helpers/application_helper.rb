3c71cdf760ce7131a1dd8b1abe20b70f3c804072
aracelicaes <aracelicaes@gmail.com>
2020-11-17 18:06:17 -0600

    Refactored Navbar and Footer View and Styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,13 @@ module ApplicationHelper
   end
 
   def categories_nav_links
-    list_item = content_tag(:li) do
+    list_item = content_tag(:div, class: 'links') do
     end
+
     category_display.collect do |category|
       list_item +=
-        content_tag(:a) do
-          link_to category_path(category), class: 'mr-3' do
+        content_tag(:div) do
+          link_to category_path(category), class: 'link' do
             category.name.upcase
           end
         end
@@ -23,49 +24,47 @@ module ApplicationHelper
     end
 
     if user_signed_in?
-      content_tag(:div, class: 'session_li') do
-        link_to new_article_path, class: 'd-flex align-items-center is-info mr-3' do
-          content_tag(:i, class: 'fa fa-plus pt-1 pr-1') do
+      content_tag(:div) do
+        link_to new_article_path, class: 'newArticle link link_icon' do
+          content_tag(:i, class: 'icon fa fa-plus') do
           end +
-            content_tag(:small, class: 'pt-1') do
-              'ARTICLE'
-            end
+          content_tag(:small) do
+            'ARTICLE'
+          end
         end
       end +
-        content_tag(:div, class: 'session_li pt-1') do
-          link_to current_user, class: 'd-flex align-items-center is-info mr-2' do
-            content_tag(:i, class: 'fa fa-user pr-1', aria: { hidden: true }) do
-            end +
-              content_tag(:small) do
-                current_user.username.upcase
-              end
-          end
-        end +
-        content_tag(:div, class: 'session_li mr-2 s_padding') do
-          link_to destroy_user_session_path, method: :delete do
-            'SIGN OUT'
-          end
-        end +
-        content_tag(:div, class: 'session_li mr-2') do
-          content_tag(:i, class: 'session_ic fa fa-search pt-1') do
+      content_tag(:div) do
+        link_to current_user, class: 'username link link_icon' do
+          content_tag(:i, class: 'icon fa fa-user', aria: { hidden: true }) do
+          end +
+          content_tag(:small) do
+            current_user.username.upcase
           end
         end
+      end +
+      content_tag(:div) do
+        link_to destroy_user_session_path, class: 'signOut link', method: :delete do
+          content_tag(:small) do
+            'SIGN OUT'
+          end
+        end
+      end +
+      content_tag(:i, class: 'search fa fa-search') do
+      end
     else
-      content_tag(:div, class: 'session_li mr-2') do
+      content_tag(:div, class: 'session_li link') do
         link_to new_user_session_path do
           'SIGN IN'
         end
       end +
-        separator +
-        content_tag(:div, class: 'session_li mr-2') do
-          link_to new_user_registration_path do
-            'SIGN UP'
-          end
-        end +
-        content_tag(:div, class: 'session_li mr-2') do
-          content_tag(:i, class: 'session_ic fa fa-search pt-1') do
-          end
+      separator +
+      content_tag(:div, class: 'session_li link') do
+        link_to new_user_registration_path do
+          'SIGN UP'
         end
+      end +
+      content_tag(:i, class: 'session_ic fa fa-search pt-1') do
+      end
     end
   end
 end
