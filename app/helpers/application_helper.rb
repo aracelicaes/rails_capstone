f335f455c971d73c2fab2e86c405e12d12a6b399
aracelicaes <aracelicaes@gmail.com>
2020-11-17 18:22:03 -0600

    Fixing Rubocop & Stylelint Errors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,43 +28,43 @@ module ApplicationHelper
         link_to new_article_path, class: 'newArticle link link_icon' do
           content_tag(:i, class: 'icon fa fa-plus') do
           end +
-          content_tag(:small) do
-            'ARTICLE'
-          end
+            content_tag(:small) do
+              'ARTICLE'
+            end
         end
       end +
-      content_tag(:div) do
-        link_to current_user, class: 'username link link_icon' do
-          content_tag(:i, class: 'icon fa fa-user', aria: { hidden: true }) do
-          end +
-          content_tag(:small) do
-            current_user.username.upcase
+        content_tag(:div) do
+          link_to current_user, class: 'username link link_icon' do
+            content_tag(:i, class: 'icon fa fa-user', aria: { hidden: true }) do
+            end +
+              content_tag(:small) do
+                current_user.username.upcase
+              end
           end
-        end
-      end +
-      content_tag(:div) do
-        link_to destroy_user_session_path, class: 'signOut link', method: :delete do
-          content_tag(:small) do
-            'SIGN OUT'
+        end +
+        content_tag(:div) do
+          link_to destroy_user_session_path, class: 'signOut link', method: :delete do
+            content_tag(:small) do
+              'SIGN OUT'
+            end
           end
+        end +
+        content_tag(:i, class: 'search fa fa-search') do
         end
-      end +
-      content_tag(:i, class: 'search fa fa-search') do
-      end
     else
       content_tag(:div, class: 'session_li link') do
         link_to new_user_session_path do
           'SIGN IN'
         end
       end +
-      separator +
-      content_tag(:div, class: 'session_li link') do
-        link_to new_user_registration_path do
-          'SIGN UP'
+        separator +
+        content_tag(:div, class: 'session_li link') do
+          link_to new_user_registration_path do
+            'SIGN UP'
+          end
+        end +
+        content_tag(:i, class: 'session_ic fa fa-search pt-1') do
         end
-      end +
-      content_tag(:i, class: 'session_ic fa fa-search pt-1') do
-      end
     end
   end
 end
